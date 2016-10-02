7bb372eb3c4bb3a60d08282f59843803024ab68a
raghupathy23 <raghupathy0123@gmail.com>
2016-10-02 20:08:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 	var r = 10;
 	var PI = 3.14;
 	
-	// To display the value of area of circle, if the radius is equal to 10
+	// To display the value of area of circle, Radius = 10
 	console.log("Area of Circle, if Radius is 10 = " +PI*r**2);
 	
